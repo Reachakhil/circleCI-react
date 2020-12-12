691d64ca0b7a6baadc11c220857aefafbed17743
Reachakhil <41184471+Reachakhil@users.noreply.github.com>
2020-12-12 23:20:50 +0530

    changed home

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '../reactcicd/build')));
 
 
 
-app.get('/', (req,res) => {
+app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../reactcicd/build/index.html'));
 });
 
