3c2cc9a1a8a7782d32dd8f5bfd25504c29ab0b79
Reachakhil <41184471+Reachakhil@users.noreply.github.com>
2021-02-02 13:37:03 +0530

    testing changes

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express(),
-      bodyParser = require("body-parser");
-      port = 3000;
-
-// place holder for the data
-const users = [];
-
-app.use(bodyParser.json());
+port = process.env.PORT ||  3000;
 app.use(express.static(path.join(__dirname, '../reactcicd/build')));
-
-
-
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../reactcicd/build/index.html'));
 });
-
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
